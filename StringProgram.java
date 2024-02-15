3e73e3bec8b5b65a7156ae617a45b9ec9a3c0464
viveksundarraj <viveksundar4@gmail.com>
2024-02-15 08:08:34 +0530

    String Changed

--- StringProgram.java
+++ StringProgram.java
@@ -3,6 +3,9 @@ class StringProgram
     public static void main(String[] args)
     {
        String str="Hi Iam Vivek";
+     
+      str="iam VivekSundarraj";
       System.out.println(str);
+
     }
   }
